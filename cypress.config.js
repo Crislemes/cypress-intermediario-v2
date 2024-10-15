179f7e2077d1bf733c511a6c344926e9f1793940
Cristiane Lemes <cristimaria077@gmail.com>
2024-10-15 11:54:50 -0300

    Criação do projeto via API

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,14 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
   },
   fixturesFolder: false,
   video: false,
+video: false,
 })
+
 
